266378572b5bdf75896bf4d607488eea8fe1ad8e
JosephShin <joespam9999@gmail.com>
2022-04-06 14:53:47 -0400

    commit tree cc2b603e0b149df1d3cfbdab0469b5e94c58087a

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // code your solution here
+//function superbowlWin(records){
+//    if (records.find(winYear)===true){
+//        return records.year
+//    }
+//}
+
+const test = [
+    {year: "2015", result: "L"},
+    {year: "2014", result: "W"}
+]
+
+/*function superbowlWin(object){
+    let result = object.find(winYear)
+    debugger
+    if(result){
+        console.log(result.year)
+        return result.year
+        
+    }else
+        return undefined
+}*/
+
+/*
+//main function
+function superbowlWin(object){
+    if(object.find(winYear)){
+        return object.find(winYear).year
+    }
+}
+
+//callback function
+function winYear(object){
+    return object.result === "W"
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+function superbowlWin(record){
+    debugger
+    return record.find(function(element) { 
+        debugger
+        if(element.result === "W"){
+            return element.year 
+        }
+            //console.log(element.year)
+            //let test2 = element.year
+            //return element.year
+        
+    }).year
+    
+}
+       
+        
